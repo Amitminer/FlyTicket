e9ed0c17cc6d529f7d00cae5c2123fd5807a9b3b
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-05-09 14:08:58 +0530

    update FlyTicketManager.php

--- src/FlyTicket/Manager/FlyTicketManager.php
+++ src/FlyTicket/Manager/FlyTicketManager.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace FlyTicket\Manager;
+
+use FlyTicket\Main;
+use pocketmine\utils\TextFormat;
+
+class FlyTicketManager
+{
+    public static $array = [];
+    public const CONSOLE_USED = TextFormat::RED."Please Use this command in game!";
+}
