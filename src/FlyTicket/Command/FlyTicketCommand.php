d849053ad840f9882cb24b9b96d4f1bba3b97f65
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-05-09 14:06:23 +0530

    update FlyTicketCommand.php

--- src/FlyTicket/Command/FlyTicketCommand.php
+++ src/FlyTicket/Command/FlyTicketCommand.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace FlyTicket\Command;
+
+use FlyTicket\Form\FormManager;
+use FlyTicket\Main;
+use FlyTicket\Manager\FlyTicketManager;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+class FlyTicketCommand extends Command
+{
+
+    public function __construct() {
+        $command_name = Main::$config->get("command");
+        $command_description = Main::$config->get("command-description");
+        parent::__construct($command_name, $command_description, "/".$command_name);
+    }
+    public function execute(CommandSender $player, string $commandLabel, array $args) {
+        if ($player instanceof Player) {
+            $formM = new FormManager();
+            $formM->FlyTicketForm($player);
+        } else {
+            $player->sendMessage(FlyTicketManager::CONSOLE_USED);
+        }
+    }
+}
