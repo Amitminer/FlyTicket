bb1d8ad38abfef4379cee8ab963ea07ab4247b5c
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-07-11 22:42:12 +0530

    Update DatabaseManager.php

--- src/FlyTicket/Manager/DatabaseManager.php
+++ src/FlyTicket/Manager/DatabaseManager.php
@@ -8,12 +8,19 @@ use poggit\libasynql\DataConnector;
 use Closure;
 use poggit\libasynql\libs\SOFe\AwaitGenerator\Await;
 
+/**
+ * Class DatabaseManager
+ * Manages the database operations for FlyTicket plugin.
+ */
 class DatabaseManager
 {
     private $database;
 
-    public function __construct() {
-
+    /**
+     * DatabaseManager constructor.
+     */
+    public function __construct()
+    {
         $plugin = Flyticket::getInstance();
         $this->database = libasynql::create($plugin, $plugin->getConfig()->get("database"), [
             "sqlite" => "database/sqlite.sql",
@@ -22,10 +29,10 @@ class DatabaseManager
     }
 
     /**
-    * Get the database connection.
-    *
-    * @param bool $load Whether to load the database connection or close it.
-    */
+     * Get the database connection.
+     *
+     * @param bool $load Whether to load the database connection or close it.
+     */
     public function getDatabase(bool $load = true): void
     {
         if ($load) {
@@ -36,32 +43,29 @@ class DatabaseManager
     }
 
     /**
-    * Load the database and initialize necessary tables.
-    */
-    private function loadDatabase() {
+     * Load the database and initialize necessary tables.
+     */
+    private function loadDatabase()
+    {
         $this->database->executeGeneric("flyticket.createTable");
     }
 
     /**
-    * Close the database connection.
-    */
-    private function closeDatabase() {
+     * Close the database connection.
+     */
+    private function closeDatabase()
+    {
         if (isset($this->database)) {
             $this->database->close(); // Close the database connection
         }
     }
 
     /**
-    * Add a player to the database.
-    *
-    * @param string $playerName The name of the player to add.
-    */
-    /**
-    * Add a player to the database.
-    *
-    * @param string $playerName The name of the player to add.
-    * @return bool True if the player was successfully added, false otherwise.
-    */
+     * Add a player to the database.
+     *
+     * @param string $playerName The name of the player to add.
+     * @return bool True if the player was successfully added, false otherwise.
+     */
     public function addPlayer(string $playerName): bool
     {
         $result = $this->database->executeInsert("flyticket.addPlayer", [
@@ -73,29 +77,34 @@ class DatabaseManager
     }
 
     /**
-    * Get player data from the database.
-    *
-    * @param string $playerName The name of the player.
-    * @return array|null The player data or null if not found.
-    */
-    public function playerExists(string $playerName) :\Generator {
-        $this->database->executeSelect("flyticket.getPlayer", [
+     * Check if a player exists in the database.
+     *
+     * @param string $playerName The name of the player.
+     * @return bool True if the player exists, false otherwise.
+     */
+    public function playerExists(string $playerName): \Generator
+    {
+        $result = yield $this->database->executeSelectSingle("flyticket.getPlayer", [
             "playerName" => $playerName
-        ], yield, yield Await::REJECT);
-        return yield Await::ONCE;
+        ]);
+
+        return $result !== null;
     }
 
-
+    /**
+     * Remove a player from the database.
+     *
+     * @param string $playerName The name of the player to remove.
+     * @return bool True if the player was successfully removed, false otherwise.
+     */
     public function removePlayer(string $playerName): bool
     {
         $result = false;
-        $this->database->executeChange("flyticket.removePlayer",
-            [
-                "playerName" => $playerName
-            ],
-            function(int $rowCount) use (&$result) {
-                $result = $rowCount > 0;
-            });
+        $this->database->executeChange("flyticket.removePlayer", [
+            "playerName" => $playerName
+        ], function (int $rowCount) use (&$result) {
+            $result = $rowCount > 0;
+        });
 
         return $result;
     }
