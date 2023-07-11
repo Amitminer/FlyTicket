f2f80108db76ba061b7648a6e9593cf019e144d7
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-07-11 22:39:51 +0530

    improved MessageManager!

--- src/FlyTicket/Manager/MessageManager.php
+++ src/FlyTicket/Manager/MessageManager.php
@@ -4,64 +4,129 @@ namespace FlyTicket\Manager;
 
 use FlyTicket\Main;
 
+/**
+ * Class MessageManager
+ * Provides methods to retrieve messages from the plugin's configuration.
+ */
 class MessageManager
 {
-    public function __construct() {
+    /**
+     * MessageManager constructor.
+     */
+    public function __construct()
+    {
         // NOOP
     }
 
-    public static function purchase(){
-        $msg = self::getConfigData("purchase-message");
-        return $msg;
+    /**
+     * Retrieves the purchase message from the configuration.
+     *
+     * @return string The purchase message.
+     */
+    public static function purchase(): string
+    {
+        return self::getConfigData("purchase-message");
     }
-    public static function usage(){
-        $msg = self::getConfigData("usage-message");
-        return $msg;
-    }
-    public static function flytimemsg(){
-        $msg = self::getConfigData("fly-time-message");
-        return $msg;
-    }
-    public static function alreadyActive(){
 
-        $msg = self::getConfigData("already-active");
-
-        return $msg;
+    /**
+     * Retrieves the usage message from the configuration.
+     *
+     * @return string The usage message.
+     */
+    public static function usage(): string
+    {
+        return self::getConfigData("usage-message");
     }
-    public static function warning(){
 
-        $msg = self::getConfigData("warning-title");
+    /**
+     * Retrieves the fly time message from the configuration.
+     *
+     * @return string The fly time message.
+     */
+    public static function flytimemsg(): string
+    {
+        return self::getConfigData("fly-time-message");
+    }
 
-        return $msg;
+    /**
+     * Retrieves the already active message from the configuration.
+     *
+     * @return string The already active message.
+     */
+    public static function alreadyActive(): string
+    {
+        return self::getConfigData("already-active");
     }
-    public static function InvFull(){
 
-        $msg = self::getConfigData("inv-full");
+    /**
+     * Retrieves the warning title from the configuration.
+     *
+     * @return string The warning title.
+     */
+    public static function warning(): string
+    {
+        return self::getConfigData("warning-title");
+    }
 
-        return $msg;
+    /**
+     * Retrieves the inventory full message from the configuration.
+     *
+     * @return string The inventory full message.
+     */
+    public static function InvFull(): string
+    {
+        return self::getConfigData("inv-full");
     }
-    public static function NoMoney(){
 
-        $msg = self::getConfigData("no-enough-money");
+    /**
+     * Retrieves the insufficient money message from the configuration.
+     *
+     * @return string The insufficient money message.
+     */
+    public static function NoMoney(): string
+    {
+        return self::getConfigData("no-enough-money");
+    }
 
-        return $msg;
+    /**
+     * Retrieves the no console message from the configuration.
+     *
+     * @return string The no console message.
+     */
+    public static function NoConsole(): string
+    {
+        return self::getConfigData("no-console");
     }
-    public static function NoConsole(){
-        $msg = self::getConfigData("no-console");
-        return $msg;
+
+    /**
+     * Retrieves the on expire message from the configuration.
+     *
+     * @return string The on expire message.
+     */
+    public static function OnExpire(): string
+    {
+        return self::getConfigData("on-expire");
     }
-    public static function OnExpire(){
-        $msg = self::getConfigData("on-expire");
-        return $msg;
+
+    /**
+     * Retrieves the fly usage message from the configuration.
+     *
+     * @return string The fly usage message.
+     */
+    public static function fly_usage(): string
+    {
+        return self::getConfigData("fly-usage");
     }
-    public static function fly_usage(){
-        $msg = self::getConfigData("fly-usage");
-        return $msg;
-    }
-    
-    private static function getConfigData(string $configdata) {
-        $config = Main::getInstance()->getConfig()->get($configdata);
-        return $config;
-     //   var_dump($config);
+
+    /**
+     * Retrieves the configuration data based on the key.
+     *
+     * @param string $configData The key to retrieve the configuration data.
+     * @return string The configuration data.
+     */
+    private static function getConfigData(string $configData): string
+    {
+        $config = Main::getInstance()->getConfig()->get($configData);
+        return (string) $config;
     }
 }
