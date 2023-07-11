0dbe4442fcd1c1afddb2e4d57a374ece3f14dcb4
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-07-11 22:43:40 +0530

    Update FlyTicketListener.php

--- src/FlyTicket/Listener/FlyTicketListener.php
+++ src/FlyTicket/Listener/FlyTicketListener.php
@@ -18,54 +18,77 @@ use pocketmine\Server;
 use pocketmine\world\sound\FireExtinguishSound;
 use poggit\libasynql\libs\SOFe\AwaitGenerator\Await;
 
+/**
+ * Class FlyTicketListener
+ * Listens to player item use events and handles FlyTicket logic.
+ */
 class FlyTicketListener implements Listener
 {
     private $databaseManager;
 
-    public function __construct() {
+    /**
+     * FlyTicketListener constructor.
+     */
+    public function __construct()
+    {
         $this->databaseManager = new DatabaseManager();
     }
-    public function itemUse(PlayerItemUseEvent $event) {
+
+    /**
+     * Handle the player item use event.
+     *
+     * @param PlayerItemUseEvent $event The event instance.
+     */
+    public function itemUse(PlayerItemUseEvent $event): void
+    {
         $item = $event->getItem();
         $player = $event->getPlayer();
         $playerName = $player->getName();
+
         if ($item->getNamedTag()->getTag("second")) {
             $second = $item->getNamedTag()->getInt("second");
-            Await::f2c(function () use ($player,$playerName,$item,$second,$event) {
-                $rows = (array)yield from $this->databaseManager->playerExists($playerName);
+            
+            Await::f2c(function () use ($player, $playerName, $item, $second, $event) {
+                $rows = (array) yield from $this->databaseManager->playerExists($playerName);
+
                 if (!empty($rows)) {
                     $event->cancel();
-                    $player->sendMessage(MessageManager::alreadyactive());
+                    $player->sendMessage(MessageManager::alreadyActive());
                     return;
                 } else {
-                    $this->executeflyticket($player,$playerName,$item,$second);
+                    $this->executeFlyTicket($player, $playerName, $item, $second);
                 }
             });
         }
     }
-    private function executeflyticket($player,$playerName,$item,$second): void {
 
-        $motion = new Vector3(0,1,0);
+    /**
+     * Execute the fly ticket logic.
+     *
+     * @param Player $player The player instance.
+     * @param string $playerName The name of the player.
+     * @param $item The item used.
+     * @param int $second The duration of the fly ticket.
+     */
+    private function executeFlyTicket(Player $player, string $playerName, $item, int $second): void
+    {
+        $motion = new Vector3(0, 1, 0);
         $player->setMotion($motion);
         $player->sendMessage(MessageManager::usage());
         $this->databaseManager->addPlayer($playerName);
-        $player->getWorld()->addSound($player->getPosition(),
-            new FireExtinguishSound(),
-            [$player]);
+        $player->getWorld()->addSound($player->getPosition(), new FireExtinguishSound(), [$player]);
         $player->getInventory()->removeItem($item);
         $time = $second / 60;
         $userName = '"' . $player->getName() . '"';
         $server = Server::getInstance();
         // TODO: USE RankSystem api
         // TODO: SUPPORT CUSTOM ITEMS AS FLYTICKET.
-        //TODO: use timer api to manage time!
+        // TODO: Use timer API to manage time!
         // CREATE MessageManager
         $command = "ranks setpermission " . $userName . " blazinfly.command " . $time . "m";
         //var_dump($command);
-        $server->dispatchCommand(new ConsoleCommandSender($server, $server->getLanguage()),
-            $command);
-        $player->sendMessage(MessageManager::fly_usage());
-        Main::$main->getScheduler()->scheduleRepeatingTask(new FlyTicketTask($player, (int)$second),
-            20 * 1);
+        $server->dispatchCommand(new ConsoleCommandSender($server, $server->getLanguage()), $command);
+        $player->sendMessage(MessageManager::flyUsage());
+        Main::$main->getScheduler()->scheduleRepeatingTask(new FlyTicketTask($player, (int) $second), 20 * 1);
     }
 }
