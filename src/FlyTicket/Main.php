be480bd7374330300d73968e51073c4e131ae0ba
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-07-11 21:03:40 +0530

    updating to pm5!

--- src/FlyTicket/Main.php
+++ src/FlyTicket/Main.php
@@ -4,24 +4,52 @@ namespace FlyTicket;
 
 use FlyTicket\Command\FlyTicketCommand;
 use FlyTicket\Listener\FlyTicketListener;
+use FlyTicket\Manager\DatabaseManager;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\utils\Filesystem;
 
 class Main extends PluginBase
 {
     public static Main $main;
-    public static Config $config;
+    private $config;
+    private $databaseManager;
+
+    public static function getInstance(): Main
+    { 
+        return self::$main;
+    }
+
     protected function onLoad(): void
     {
         self::$main = $this;
         $this->saveResource("config.yml");
-        self::$config = new Config($this->getDataFolder()."config.yml", 2);
+        $this->config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
     }
+
     public function onEnable(): void
     {
-        $command_name = self::$config->get("command");
+        $this->deleteOldDataFile();
+        $this->saveDefaultConfig();
+        $this->databaseManager = new DatabaseManager();
+        $this->databaseManager->getDatabase(true);
+        $command_name = $this->config->get("command");
         $this->getLogger()->info("FlyTicket Enabled!");
         $this->getServer()->getCommandMap()->register($command_name, new FlyTicketCommand());
         $this->getServer()->getPluginManager()->registerEvents(new FlyTicketListener(), $this);
+    }
+
+    public function onDisable(): void
+    {
+        $this->databaseManager->getDatabase(false);
+        $this->deleteOldDataFile();
+    }
+    
+    public function deleteOldDataFile(): void {
+        $dataFilePath = $this->getDataFolder() . 'flyticketData.db' ?? null;
+        if (file_exists($dataFilePath)) {
+            unlink($dataFilePath);
+            $this->getLogger()->info("§aflyticketData deleted");
+        } else {}
     }
 }
